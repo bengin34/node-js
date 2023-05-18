7f7ba9007b28b80c7d9eccd9d700d017cd2af983
bengin34 <becaglar3434@gmail.com>
2023-05-18 20:01:23 +0300

    products

--- 3-projects/02-store/controllers/products.js
+++ 3-projects/02-store/controllers/products.js
@@ -1,14 +1,26 @@
+const Product = require("../models/product");
 
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    page: "2",
+  });
+  res.status(200).json({ products, nbHits: products.length });
+};
 
-const getAllProductsStatic = async(req,res)=> {
-    res.status(200).json({msg:'product testing route'})
-}
+const getAllProducts = async (req, res) => {
+  const { featured } = req.query;
+  const queryObject = {}
 
-const getAllProducts = async(req,res)=> {
-    res.status(200).json({msg:'products route'})
-}
+  if(featured){
+    queryObject.featured = featured === 'true' ? true : false
+  }
+
+  const products = await Product.find(queryObject);
+  console.log(queryObject)
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 module.exports = {
-    getAllProductsStatic,
-    getAllProducts,
-}
+  getAllProductsStatic,
+  getAllProducts,
+};
