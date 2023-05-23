2fde23d5b6871bdfe408e9a3ad8178c770849e72
bengin34 <becaglar3434@gmail.com>
2023-05-23 13:20:06 +0300

    links

---
+++ 3-projects/New Project/client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link>Home</Link>
+      <Link>Login</Link>
+      <Link>Register</Link>
+    </div>
+  );
+};
+
+export default Navbar;
